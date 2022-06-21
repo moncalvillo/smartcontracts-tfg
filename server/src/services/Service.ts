07cc9ca8d437076142fbc78907a75c378eb11416
pabgonmon2 <pablogonmon@gmail.com>
2022-06-21 22:32:12 +0200

    Service template

---
+++ server/src/services/Service.ts
@@ -0,0 +1,14 @@
+import IService from "./IService";
+
+
+export class Service extends IService{
+    constructor(){
+        super()
+    }
+
+    
+}
+
+const service = new Service();
+
+export default service;
