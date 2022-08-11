4cad1a23affc082d51f03ee45093a601901816a6
Pablo González Moncalvillo <moncalvillo.pablo@gmail.com>
2022-08-12 00:50:16 +0200

    feat - Added role options in register form

--- client/src/components/forms/RegisterForm.jsx
+++ client/src/components/forms/RegisterForm.jsx
@@ -11,10 +11,14 @@ const RegisterForm = ({onRegisterRedirect}) => {
     const [confirmPassword, setConfirmPassword] = useState("");
     const [username, setUsername] = useState("");
     const [email, setEmail] = useState("");
+    const [firstname, setFirstname] = useState("");
+    const [lastname, setLastname] = useState("");
+    const [role, setRole] = useState("");
+
     const [error, setError] = useState(null);
     const [disabled, setDisabled] = useState(false);
 
-    const {setAccessToken} = useUser();
+    const {setUser} = useUser();
 
 
 
@@ -24,17 +28,16 @@ const RegisterForm = ({onRegisterRedirect}) => {
 
     const handleSubmit = async () => {
         setLoader(true);
-        axios.post("http://localhost:8080/api/server/new", {email: email, username: username, password: password}).then((res) => {
+        axios.post("/server/new", {email, username, password, firstName: firstname, lastName: lastname, roleType: role}).then((res) => {
             if(res.status === 201) {
-                setAccessToken(res.data.accessToken);
-                document.cookie = `accessToken=${res.data.accessToken}`;
-            }else{
-                
+                setUser(res.data.user);
+                // document.cookie = `accessToken=${res.data.accessToken}`;
             }
         }).catch((err) => {
             console.log(err.response);
             setError(err.response.data.message);
         }).finally(() => {
+            setLoader(false);
             setTimeout(()=>{
                 setLoader(false);
             }, 1000)
@@ -55,6 +58,11 @@ const RegisterForm = ({onRegisterRedirect}) => {
         }
     }, [confirmPassword,password])
 
+    const onValueChange = (e) => {
+        setRole(e.target.value);
+        console.log(e.target.value);
+    }
+
     return (
         <div className="formDiv">
             
@@ -71,29 +79,59 @@ const RegisterForm = ({onRegisterRedirect}) => {
                 <div className="box">
                     <h2>Register</h2>
                     <label htmlFor="email">
-                    Email
-                    <input required className="input" type="email" name="email" placeholder="email" onBlur={(e)=>{
-                        setEmail(e.target.value);
-                    }}/>
-                    </label>
+                        Email
+                        <input required className="input" type="email" name="email" placeholder="email" onBlur={(e)=>{
+                            setEmail(e.target.value);
+                        }}/>
+                        </label>
                     <label htmlFor="username">
-                    Username
-                    <input required className="input" type="text" name="username" placeholder="username" onBlur={(e)=>{
-                        setUsername(e.target.value);
-                    }}/>
+                        Username
+                        <input required className="input" type="text" name="username" placeholder="username" onBlur={(e)=>{
+                            setUsername(e.target.value);
+                        }}/>
+                        </label>
+                    <div className="fullname">
+                        <label htmlFor="firstname">
+                            First name
+                            <input required className="input" type="text" name="firstname" placeholder="first name" onBlur={(e)=>{
+                                setFirstname(e.target.value);
+                            }}/>
+                        </label>
+                        <label htmlFor="lastname">
+                            Last name
+                            <input required className="input" type="text" name="lastname" placeholder="last name" onBlur={(e)=>{
+                                setLastname(e.target.value);
+                            }}/>
+                        </label>
+                    </div>
+                    <label htmlFor="role">
+                    Role
+                        <div className="wrapper" >
+                            <input type="radio" name="role" id="user" value="user" checked={role === "user"}
+              onChange={onValueChange}/>
+                            <input type="radio" name="role" id="manager" value="manager" checked={role === "manager"}
+              onChange={onValueChange}/>
+                            <label for="user" className="option user">
+                                <div class="dot"></div>
+                                <span>Responsible</span>
+                            </label>
+                            <label for="manager" className="option manager">
+                                <div class="dot"></div>
+                                <span>Manager</span>
+                            </label>
+                        </div>
                     </label>
-                        
                     <label htmlFor="password">
-                    Password
-                    <input required className="input" type="password" name="password" placeholder="password" onBlur={(e)=>{
-                        setPassword(e.target.value);
-                    }}/>
+                        Password
+                        <input required className="input" type="password" name="password" placeholder="password" onBlur={(e)=>{
+                            setPassword(e.target.value);
+                        }}/>
                     </label>
                     <label htmlFor="rptPassword">
-                    Repeat password
-                    <input required className="input" type="password" name="rptPassword" placeholder="repeat password" onChange={(e) => {
-                        setConfirmPassword(e.target.value);
-                    }}/>
+                        Repeat password
+                        <input required className="input" type="password" name="rptPassword" placeholder="repeat password" onChange={(e) => {
+                            setConfirmPassword(e.target.value);
+                        }}/>
                     </label>
                     Already have an account? <a onClick={onRegisterRedirect}>Login</a>
                 </div>
