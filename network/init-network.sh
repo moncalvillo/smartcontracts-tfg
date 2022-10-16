a7a764cdaede8e3cf9d3a1dc5bed22d8a6ac6d1c
Pablo Gonzalez Moncalvillo <pabgonmon@gmail.com>
2022-10-16 22:28:30 +0200

    hyperledger image

---
+++ network/init-network.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+./install-fabric.sh --fabric-version 2.4.6 --ca-version 1.5.5
+
+docker images
+
+cd fabric-samples
+cd Chaincodes/draft-typescript
+npm install
+
+cd ../../test-network
+export PATH=${PWD}/../bin:$PATH
+export FABRIC_CFG_PATH=$PWD/../config/
+
+./network.sh down
+./network.sh up createChannel -ca -s couchdb
+# ./network.sh deployCC -ccn draft -ccp ../Chaincodes/draft-typescript/ -ccl typescript
